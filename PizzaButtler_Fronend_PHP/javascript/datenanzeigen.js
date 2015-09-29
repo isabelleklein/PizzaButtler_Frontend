b2c777fdcc172611fa684e5f0ae2f644141fc524
Flexon94 <felix.tebruegge@t-online.de>
2015-09-29 19:17:57 +0200

    Seite Daten anzeigen/ändern
    Erster Entwurf der beiden Seiten.

---
+++ PizzaButtler_Fronend_PHP/javascript/datenanzeigen.js
@@ -0,0 +1,29 @@
+
+
+function machwas() {
+	console.log("Button gedrückt");
+	window.location.href = "datenaendern.html";
+}
+
+
+
+/**$(document).ready(function() {
+	var $datenanzeigen = $('#datenanzeigen');
+	/** Aktionsinformationen für den Absenden-Button 
+	
+	$('#datenaendern').click(function(e) {
+		console.log("Button wurde geklickt");
+		
+	});
+});
+**/
+
+
+
+/**
+	if (document.getElementById("datenaendern").onclick==true){
+	console.log("test");
+	}
+	
+
+**/
