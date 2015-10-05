4bc6240ebdd051372065235ee4ada17919d0963d
dominik-ttt <dominik-ttt@gmx.de>
2015-10-05 10:24:36 +0200

    Farben nach Designvorgabe

--- PizzaButtler_Fronend_PHP/javascript/kundenregistrierung.js
+++ PizzaButtler_Fronend_PHP/javascript/kundenregistrierung.js
@@ -236,7 +236,7 @@ function agbPruefen()
 function fehlerAusgeben(fehl, feld)
 {
     document.getElementById("" + fehl).style.display="inline"; //Fehlerhinweis wird angezeigt
-    document.getElementById("" + feld).style.background="#852002"; //Fehlerhaftes Feld wird markiert
+    document.getElementById("" + feld).style.background="#842002"; //Fehlerhaftes Feld wird markiert
     document.getElementById("" + feld).style.color="#ffffff"; //Schriftfarbe des fehlerhaften Feldes wird dem neuen Hintergrund angepasst
     document.getElementById("container").style.height="320"; //Pop-Up wird zur Angabe des Fehlers vergrößert
 	}
@@ -244,7 +244,7 @@ function hinweisVerbergen(fehl, feld)
 {
 	  document.getElementById("" + fehl).style.display="none"; //Fehlerhinweis wird ausgeblendet
 	  document.getElementById("" + feld).style.background="#ffffff"; //Fehlerhaftes Feld wird farblich in Ausgangszustand gebracht
-	  document.getElementById("" + feld).style.color="#852002"; //Schriftfarbe wird farblich in Ausgangszustand gebracht
+	  document.getElementById("" + feld).style.color="#842002"; //Schriftfarbe wird farblich in Ausgangszustand gebracht
 	  document.getElementById("container").style.height="300"; //Die Größe des Pop-Ups wird in Ausgangszustand gebracht
 	}
 
