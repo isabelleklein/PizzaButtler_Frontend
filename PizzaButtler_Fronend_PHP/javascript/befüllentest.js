d29938a50f91d2f5a0b7700e6e0b1b0b35a9c581
ChFlick <christophflick@gmx.de>
2015-10-06 10:41:03 +0200

    Profildaten anzeigen implementiert

--- PizzaButtler_Fronend_PHP/javascript/befüllentest.js
+++ PizzaButtler_Fronend_PHP/javascript/befüllentest.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 	getDaten(function(daten) {
 		console.log(daten);
-		document.getElementById('vorname').innerHTML = $('#vorname').val(daten.vorname);
+		$('#anrede').text(daten.anrede);
+		$('#vorname').text(daten.vorname);
+		$('#nachname').text(daten.nachname);
+		$('#strasse').text(daten.strasse);
+		$('#hausnummer').text(daten.hausnummer);
+		$('#plz').text(daten.plz);
+		$('#ort').text(daten.ort);
+		$('#email').text(daten.email);
+		$('#telefonnummer').text(daten.telefonnummer);
 		
 	});
 });
