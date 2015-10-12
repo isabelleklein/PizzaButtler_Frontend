3b2b1dd78902d6effe147458e9f06bc29c4e5f22
Jules93 <juliansteil@gmail.com>
2015-10-12 11:20:07 +0200

    Login verbessert

--- PizzaButtler_Fronend_PHP/javascript/login_absenden.js
+++ PizzaButtler_Fronend_PHP/javascript/login_absenden.js
@@ -1,32 +1,24 @@
     
 $(function() {
-	var $registrieren = $('#login');   
-	$('#absenden').click(function(e){
-		console.log("Login Button wurde geklickt");
-		if(checkForm()==true)
-		{
-		e.preventDefault(); /** cancel form submit **/
-		var jsObj = 
-	})
+	$('#openx').click(
+	        function() {
+	        	console.log("1");
+	            $('#overlay').show('slow', 
+	                function() {
+	                    $('#container').fadeIn('slow');
+	                    $('#changeText').html('Dynamischer Inhalt');
+	                }
+	            );
+	        }
+	    );
+	$('#closex').click(
+	            function() {
+	                $('#container').hide('slow', 
+	                     function() {
+	                          $('#overlay').fadeOut();          
+	                     }    
+	                );
+	            }
+	        );
 
-$('#open').click(
-        function() {
-        	console.log("1");
-            $('#overlay').show('slow', 
-                function() {
-                    $('#container').fadeIn('slow');
-                    $('#changeText').html('Dynamischer Inhalt');
-                }
-            );
-        }
-    );
-$('#close').click(
-            function() {
-                $('#container').hide('slow', 
-                     function() {
-                          $('#overlay').fadeOut();          
-                     }    
-                );
-            }
-        );
 });
