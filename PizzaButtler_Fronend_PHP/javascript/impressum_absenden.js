072852a144ab4651b4d40ac6e3b66e9bb9da1e0e
Isabelle Klein <Isa@Isabelles-Air.fritz.box>
2015-10-17 21:38:13 +0200

    AGB/Impressum/Datenschutz
    Das Impressum habe ich als PopUp gestaltet die anderen Seiten waren
    dafür zu groß. Design-Technisch ist das noch nicht abgestimmt und wird
    noch verändert.

---
+++ PizzaButtler_Fronend_PHP/javascript/impressum_absenden.js
@@ -0,0 +1,40 @@
+/**
+ * @author Dominik Terlau
+ * @Version 2.1
+ */
+
+
+/** Übermittlung der eingegebenen Daten an das Backend **/
+
+$(document).ready(function() {
+	var $impressum = $('#impressum');
+	/** Aktionsinformationen für den Absenden-Button **/
+	
+
+	
+    $('#open').click(
+        function() {
+            $('#overlay').show('slow', 
+                function() {
+                    $('#container').fadeIn('slow');
+                    $('#changeText').html('Dynamischer Inhalt');
+                }
+            );
+        }
+    ); 
+    
+     $('#close').click(
+        function() {
+            $('#container').hide('slow', 
+                 function() {
+                      $('#overlay').fadeOut();          
+                 }    
+            );
+        }
+    );
+     
+
+});
+
+
+
