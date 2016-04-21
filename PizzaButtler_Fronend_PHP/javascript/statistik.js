eddd716f090abf744a727c0ee2f26372b6c01a33
ChFlick <christophflick@gmx.de>
2016-04-21 23:17:40 +0200

    Bugfixes(Fließkomma) + Rechschreibfixes + Bestellungenseite vorbereitet

--- PizzaButtler_Fronend_PHP/javascript/statistik.js
+++ PizzaButtler_Fronend_PHP/javascript/statistik.js
@@ -15,25 +15,25 @@ var statistikAufrufen = function(data){
 	var anzKunden = data.kundenSUM;
 	var umsavg = data.umsatzAVG;
 	
-	$("#anzahlKunden").html(anzKunden + "Kunden");
+	$("#anzahlKunden").html(anzKunden + " Kunden");
 	$("#anzahlBestellungen").html(bestellungen + " Bestellungen im aktuellen Monat");
-	$("#monUmsBrt").html(brutto + "€ im aktuellen Monat");
+	$("#monUmsBrt").html(brutto.toFixed(2) + "€ im aktuellen Monat");
 	/** aktuell wird davon ausgegangen, dass einfach bei jeder Bestellung 19% Mehrwertsteuer fällig ist. 
 	 * Künftig muss die Berechnung durch das Backend vorgenommen werden, um den korrekten Wert zu ermitteln
 	 */
 	var netto = brutto/1.19
 	$("#monUmsNet").html(netto.toFixed(2) + "€ im aktuellen Monat"); 
-	$("#schnittBestWert").html(umsavg + "€ pro Bestellung")
+	$("#schnittBestWert").html(umsavg.toFixed(2) + "€ pro Bestellung")
 	if(brutto < 500){
-		$("#gebuehren").html(brutto*0.125 + "€");
+		$("#gebuehren").html((brutto*0.125).toFixed(2) + "€");
 	}
 	else if(brutto < 2000){
-		$("#gebuehren").html(brutto*0.1 + "€");
+		$("#gebuehren").html((brutto*0.1).toFixed(2) + "€");
 	}
 	else if(brutto < 5000){
-		$("#gebuehren").html(brutto*0.09 + "€");
+		$("#gebuehren").html((brutto*0.09).toFixed(2) + "€");
 	}
 	else{
-		$("#gebuehren").html(brutto*0.08 + "€");
+		$("#gebuehren").html((brutto*0.08).toFixed(2) + "€");
 	}
 }
